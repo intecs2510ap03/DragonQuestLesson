db96e51c8fde367549775916b9c59c1204ceea9d
user03 <intecs.2510ap.03@gmail.com>
2026-01-14 09:53:19 +0900

    Day3: Scannerと演算子を習得

--- Day3Main.java
+++ Day3Main.java
@@ -5,15 +5,26 @@ public class Day3Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-        System.out.println("ガライの町へようこそ！");
+        System.out.println("ガライの町の道具屋へようこそ！");
         System.out.print("あなたの名前を教えてください：");
         String name = scan.nextLine(); // 文字列の入力
+        System.out.println(name + "さん、いらっしゃい！");
+        
+        System.out.println("");
 
-        System.out.print(name + "よ、薬草をいくつ買いますか？（1個8ゴールド）：");
-        int count = scan.nextInt(); // 整数の入力
+        
+        System.out.print("薬草(8G)は何個買いますか？");
+        int yakuso = scan.nextInt(); // 整数の入力
+        
+        System.out.print("毒消し草(10G)は何個買いますか？");
+        int dokuso = scan.nextInt(); // 整数の入力
+        
+        System.out.println("");
+        System.out.println("--- 計算中 ---");
 
-        int totalPrice = count * 8; // 掛け算
-        System.out.println("合計で " + totalPrice + " ゴールドになります！");
+        int totalPrice = yakuso * 8 + dokuso * 10; // 掛け算
+        System.out.println("合計金額は" + totalPrice + " ゴールドになります！");
+        System.out.println("ありがとうございました！");
         
         scan.close(); // 道具を片付ける
 
