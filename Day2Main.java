dfda39fa19f4e474e0ee7a692c43f9467db732f5
user03 <intecs.2510ap.03@gmail.com>
2026-01-14 09:39:11 +0900

    Day2: 変数の魔法を習得

---
+++ Day2Main.java
@@ -0,0 +1,16 @@
+
+public class Day2Main {
+    public static void main(String[] args) {
+        // 変数の宣言と代入
+        String heroName = "ロトの血を引く者"; // 文字列
+        int hp = 20;                      // 整数
+        double weight = 5.5;              // 小数
+        boolean isPoisoned = false;       // 真偽値
+
+        // コンソールに表示
+        System.out.println("名前：" + heroName);
+        System.out.println("体力：" + hp);
+        System.out.println("重さ：" + weight + "kg");
+        System.out.println("毒状態：" + isPoisoned);
+    }
+}
