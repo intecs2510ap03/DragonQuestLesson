3149f1f868271dbc8371b447eb145c700128751e
user03 <intecs.2510ap.03@gmail.com>
2026-01-14 11:11:57 +0900

    Day8: 伝説の武器を生成(コンストラクタ)

--- Day8Main.java
+++ Day8Main.java
@@ -4,7 +4,14 @@ public class Day8Main {
 	public static void main(String[] args) {
 		Weapon weapon = new Weapon("ロトの剣", 100);
 		
+		Weapon w2 = new Weapon("修行中の剣");
+
+		// パターン3が呼ばれる
+		Weapon w3 = new Weapon();
+		
 		System.out.println(weapon);
+		System.out.println(w2);
+		System.out.println(w3);
 
 	}
 
--- Weapon.java
+++ Weapon.java
@@ -9,6 +9,17 @@ public class Weapon {
 		this.name = name;
 		this.attack = attack;
 	}
+	// パターン2：名前だけ指定する（攻撃力は初期値 5 になる）
+    public Weapon(String name) {
+        this.name = name;
+        this.attack = 5; 
+    }
+
+    // パターン3：何も指定しない（名無しの棒きれ）
+    public Weapon() {
+        this.name = "ひのきのぼう";
+        this.attack = 1;
+    }
 
 	public String toString() {
 		return "武器「" + this.name +  "」を鍛造した！\n攻撃力：" + this.attack;
